1c6970931938e9e2b01d5fd8c760475f89b445c1
zfr <mohammadalizafar986@gmail.com>
2023-07-01 15:20:56 -0600

    js object

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let myLibrary = [];
+let myLibrary = [hungerGames = new Book('The Hunger Games', 'Suzanne Collins', 374, false)];
 
-function Book() {
+function Book(title, author, pageCount, isRead) {
   // the constructor...
+  this.title = title
+  this.author = author
+  this.pageCount = pageCount
+  this.isRead = isRead
 }
 
 function addBookToLibrary() {
