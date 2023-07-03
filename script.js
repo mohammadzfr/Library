75d4b5948951f9191a8e4e40ec5f5f088655284c
zfr <mohammadalizafar986@gmail.com>
2023-07-03 15:17:57 -0600

    added buttons for editing and removing

--- script.js
+++ script.js
@@ -3,25 +3,22 @@ const table = document.querySelector("table");
 const tbody = document.querySelector("tbody");
 const form = document.querySelector("form");
 const addBook = document.querySelector(".add-book");
-
+var btnCallRmv = document.querySelectorAll(".btn-rmv");
+var btnCallEdit = document.querySelectorAll(".btn-edit");
 // Event Listeners
+console.log(btnCallEdit);
+//when add book is clicked, open form
 addBook.addEventListener('click', function() {
   form.classList.remove("clear");
-})
+  table.classList.add("clear");
+});
 
+
+
+//when form is submitted
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  let title = document.querySelector("#title").value;
-  let author = document.querySelector("#author").value;
-  let pageCount = document.querySelector("#pages").value;
-  let isRead = document.querySelector('input[name="read"]:checked').value;
-  let book = new Book(title, author, pageCount, isRead);
-  myLibrary[myLibrary.length] = book;
-  console.log(myLibrary[myLibrary.length-1]);
-  book.displayBook();
-  console.log(title + author + pageCount + isRead);
-  form.reset();
-  form.classList.add("clear");
+  addBookToLibrary();
 });
 
 // Book stuff
@@ -33,13 +30,34 @@ function Book(title, author, pageCount, isRead) {
   // the constructor...
   this.title = title;
   this.author = author;
-  this.pageCount = pageCount;
+  this.pageCount = Number(pageCount);
   this.isRead = isRead;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  //grabs values from completed form
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pageCount = document.querySelector("#pages").value;
+  let isRead = document.querySelector('input[name="read"]:checked').value;
+  let book = new Book(title, author, pageCount, isRead);
 
+  //adds book to library
+  myLibrary[myLibrary.length] = book;
+  console.log(myLibrary[myLibrary.length-1]);
+
+  //displays book in table
+  book.displayBook();
+  console.log(title + author + pageCount + isRead);
+
+  //clear form
+  form.reset();
+  form.classList.add("clear");
+  table.classList.remove("clear");
+
+  clearLibrary();
+  displayLibrary();
+  
 }
 
 //loops through every book in myLibrary
@@ -56,6 +74,11 @@ Book.prototype.displayBook = function() {
   const entryA = document.createElement('td');
   const entryP = document.createElement('td');
   const entryR = document.createElement('td');
+  const entryBtn1 = document.createElement('td');
+  const entryBtn2 = document.createElement('td');
+  const btnEdit = document.createElement('button');
+  const btnRmv = document.createElement('button');
+  const index = myLibrary.length - 1
   tbody.appendChild(row);
   entryT.innerHTML = this.title;
   row.appendChild(entryT);
@@ -65,6 +88,39 @@ Book.prototype.displayBook = function() {
   row.appendChild(entryP);
   entryR.innerHTML = this.isRead;
   row.appendChild(entryR);
+
+  btnEdit.setAttribute("entry", index);
+  btnRmv.setAttribute("entry", index);
+  btnEdit.classList.add("btn-edit");
+  btnRmv.classList.add("btn-rmv");
+  btnEdit.innerHTML = "Edit Book";
+  btnRmv.innerHTML = "Remove Book";
+  entryBtn1.appendChild(btnEdit);
+  row.appendChild(entryBtn1);
+  
+  entryBtn2.appendChild(btnRmv);
+  row.appendChild(entryBtn2);
+
+  btnCallRmv = document.querySelectorAll(".btn-rmv");
+  btnCallEdit = document.querySelectorAll(".btn-edit");
+  console.log(btnCallEdit);
+
+  btnCallEdit.forEach(edit => edit.addEventListener('click', function() {
+    console.log("test");
+  }))
+  btnCallRmv.forEach((rmv) => {
+    rmv.addEventListener('click', () => {
+      console.log("test");
+    });
+  });
+}
+
+function clearLibrary() {
+  var node = document.querySelector("tbody");
+  while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+  }
 }
 
 displayLibrary();
+// clearLibrary();
